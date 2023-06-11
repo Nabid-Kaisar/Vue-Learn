dfbb1663944e7dc89d01abcc17de5664001c2871
Nabid Kaisar <himelkaisar023@gmail.com>
2023-06-11 16:46:44 +0600

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+  import TodoApp from "./components/TodoApp.vue"
+  import Header from "./components/Header.vue"
+  export default {
+    components: {
+      TodoApp,
+      Header
+    }
+  }
+</script>
+
+<template>
+  <Header />
+  <TodoApp />
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>This is todo app!</h2>
+</template>
